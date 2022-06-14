5e9e3815ffad08682df88a50c8fc54ce3cd9f995
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-06-14 16:36:21 +0200

    try 002_5

--- cypress/e2e/specs/002_configureProject.spec.js
+++ cypress/e2e/specs/002_configureProject.spec.js
@@ -7,8 +7,8 @@ describe('Configure Project', () => {
 
     beforeEach(() => {
         cy.createExampleProject();
-        cy.visitProjects();
-        cy.pause(6000);
+        // cy.visitProjects(); // commented in try 002_5
+        // cy.pause(10000); // commented in try 002_5
     })
 
     afterEach(() => {
@@ -22,7 +22,39 @@ describe('Configure Project', () => {
         cy.disconnectFromDappify();
     });
 
-    // // 002_2
+    // 002_1: empty beforeEach hook, creating project inside test case instead of using re-factored cy.createExampleProject();
+    // => failed
+    // it('complete config', () => {
+    //     cy.visitProjects();
+    //     cy.contains('Add Project').click();
+    //     // choose and write project name
+    //     cy.get('input').type('Example Project');
+    //     cy.contains('Continue').click();
+    //     // select chain, only first works (li:first)
+    //     cy.get('div[id="demo-simple-select"]').click().get('ul li:first').click();
+    //     cy.contains('Continue').click();
+    //     // read and accepted the terms and conditions and remove focus after it, to be able to click button
+    //     cy.get('[type="checkbox"]').check().blur();
+    //     // UI shows "Create Project", in html it has "Create project"
+    //     cy.get('button').contains('Create').click();
+    //     // wait for progress to become 100%
+    //     cy.wait(7000);
+    //     // click Continue
+    //     cy.contains('Continue').click();
+    //     // within project studio click link "My Projects"
+    //     // UI shows "Create Project", in html it has "Create project"
+    //             // cy.visitProjects();
+    //     // cy.contains('Example Project Cy').click();
+    //     // cy.wait(2000);
+    //     cy.get('textarea').type('My example description of this project.');
+    //     // check if now 40% 
+    //     cy.get('Look & Feel').focus().scroll(0,500);
+    //     cy.wait(6000);
+    //     cy.contains('40%').should('be.true');
+    //     // how to scroll in navbar
+    // })
+
+    // // 002_2 visit projects inside test case => failed
     // it('open new project', () => {
     //     cy.visitProjects();
     //     cy.contains('Example Project Cy').click();
@@ -33,7 +65,7 @@ describe('Configure Project', () => {
     //     cy.contains('40%').should('be.true');
     // })
 
-    // 002_3
+    // 002_3 wait inside test case => skipped (failed)
     // it('configure to 100percent', () => {
     //     cy.wait(10000);
     //     cy.visitProjects();
@@ -45,18 +77,37 @@ describe('Configure Project', () => {
     //     cy.contains('40%').should('be.true');
     // })
 
+    // 002_4 waiting in beforeEach hook and visiting the project overview page
+    // left out completely, not even mentioned in test runner (left panel)
+    // it('open and complete new project', () => {
+    //     cy.contains('Example Project Cy').click();
+    //     cy.get('textarea').type('My example description of this project.');
+    //     // check if now 40% 
+    //     cy.get('Look & Feel').focus().scroll(0,500).blur();
+    //     cy.wait(6000);
+    //     cy.contains('40%').should('be.true');
+    // })
 
-
-    // 002_2
+    // 002_5 no waiting, just continue from beforeEach hook with freshly new created project within the project studio
     it('open and complete new project', () => {
-        cy.visitProjects();
-        cy.contains('Example Project Cy').click();
         cy.get('textarea').type('My example description of this project.');
         // check if now 40% 
-        cy.get('Look & Feel').focus().scroll(0,500);
+        cy.get('Look & Feel').focus().scroll(0,500).blur();
         cy.wait(6000);
         cy.contains('40%').should('be.true');
     })
+
+
+    // 002_2
+    // it('open and complete new project', () => {
+    //     cy.visitProjects();
+    //     cy.contains('Example Project Cy').click();
+    //     cy.get('textarea').type('My example description of this project.');
+    //     // check if now 40% 
+    //     cy.get('Look & Feel').focus().scroll(0,500);
+    //     cy.wait(6000);
+    //     cy.contains('40%').should('be.true');
+    // })
 
     // 002_4 (cy.visitProjects(); inside before hook, same as in 001)
     // it('complete config', () => {
@@ -88,36 +139,7 @@ describe('Configure Project', () => {
     //         // how to scroll in navbar
     //     })
 
-    // 002_1
-    // it('complete config', () => {
-    //     cy.visitProjects();
-    //     cy.contains('Add Project').click();
-    //     // choose and write project name
-    //     cy.get('input').type('Example Project');
-    //     cy.contains('Continue').click();
-    //     // select chain, only first works (li:first)
-    //     cy.get('div[id="demo-simple-select"]').click().get('ul li:first').click();
-    //     cy.contains('Continue').click();
-    //     // read and accepted the terms and conditions and remove focus after it, to be able to click button
-    //     cy.get('[type="checkbox"]').check().blur();
-    //     // UI shows "Create Project", in html it has "Create project"
-    //     cy.get('button').contains('Create').click();
-    //     // wait for progress to become 100%
-    //     cy.wait(7000);
-    //     // click Continue
-    //     cy.contains('Continue').click();
-    //     // within project studio click link "My Projects"
-    //     // UI shows "Create Project", in html it has "Create project"
-    //             // cy.visitProjects();
-    //     // cy.contains('Example Project Cy').click();
-    //     // cy.wait(2000);
-    //     cy.get('textarea').type('My example description of this project.');
-    //     // check if now 40% 
-    //     cy.get('Look & Feel').focus().scroll(0,500);
-    //     cy.wait(6000);
-    //     cy.contains('40%').should('be.true');
-    //     // how to scroll in navbar
-    // })
+    
 
     // 002
     // it('Configure Project to 100%', () => {
